5ed8b46dac41c90fb3be147b8254fcfc04decc58
tori <hhans6245@gmail.com>
2024-07-22 14:46:35 +0900

    7/22일 일지 작성

--- js/index.js
+++ js/index.js
@@ -11,7 +11,27 @@ function lamp() {
         $('.lampInternal').css('box-shadow', '0 0 10px gold');
         $('.light').css('display', 'block');
         $('.lightCircle').css('display', 'block');
+
+        setTimeout(function(){
+            closeSplash();
+        }, 5000);
     });
+    
+}
+function closeSplash(){
+    $('.lampInternal').css('box-shadow', 'none');
+    $('.light').css('display', 'none');
+    $('.lightCircle').css('display', 'none');
+    
+    setTimeout(function(){
+        $('.lampBody').animate({'top' : '-150px'}, 3000, 'easeOutCubic', function(){
+            $('.splash').animate({'top' : '10%'}, 1000, 'easeOutCubic', function(){
+                $('.splash').animate({'top' : '-100%'}, 500, 'easeOutCubic', function(){
+                    $('.splash').hide();
+                });
+            });
+        });
+    }, 200);
     
 }
 
