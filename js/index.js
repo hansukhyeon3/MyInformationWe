88fdfdf83a22e4af4313bf45b183e0662604d27a
hansukhyeon3 <hhans6245@gmail.com>
2024-07-24 09:13:50 +0900

    7월 23일 화요일 수정

--- js/index.js
+++ js/index.js
@@ -8,30 +8,36 @@ $(function(){
 
 function lamp() {
     $('.lampBody').animate({'top' : '150px'}, 3000, 'easeOutCubic', function(){
-        $('.lampInternal').css('box-shadow', '0 0 10px gold');
-        $('.light').css('display', 'block');
-        $('.lightCircle').css('display', 'block');
+        $('.lampInternal').css('box-shadow', '0 0 10px rgba(153, 116, 23, 1)');
+        $('.light').show();
+        $('.lightGround').show();
+        $('.splashLogo').addClass('splashLogoOffBlur');
 
         setTimeout(function(){
             closeSplash();
-        }, 5000);
+        }, 2000);
     });
     
 }
 function closeSplash(){
-    $('.lampInternal').css('box-shadow', 'none');
-    $('.light').css('display', 'none');
-    $('.lightCircle').css('display', 'none');
-    
-    setTimeout(function(){
+    $('.splashLogo').removeClass('splashLogoOffBlur').addClass('splashLogoOnBlur').animate({'opacity' : '0'}, 3000);
+    $('.light').animate({'opacity' : '0'}, 3000);
+    $('.lampInternal').addClass('lampInternalOff');
+    $('.lightGround').animate({'opacity' : '0'}, 3000, function(){
         $('.lampBody').animate({'top' : '-150px'}, 3000, 'easeOutCubic', function(){
             $('.splash').animate({'top' : '10%'}, 1000, 'easeOutCubic', function(){
                 $('.splash').animate({'top' : '-100%'}, 500, 'easeOutCubic', function(){
                     $('.splash').hide();
+                    startIndex();
                 });
             });
         });
-    }, 200);
-    
+    });
 }
 
+function startIndex(){
+    $('.mainWrap').addClass('mainWrapRemoveBlur');
+    setTimeout(function(){
+        $('header').css('box-shadow' , 'rgba(153, 116, 23, 1) 0 10px 20px');
+    }, 1500);
+}
