6cde2597a7577bf92f470f7da38e54e3ad50297f
tori <hhans6245@gmail.com>
2024-08-07 14:02:13 +0900

    8월7일

--- js/resume.js
+++ js/resume.js
@@ -1,3 +1,12 @@
 $(function(){
-
+    openBook();
 });
+
+function openBook() {
+    $('.bookCover').click(function(){
+        $('.bookPage').css('transform', 'rotateY(-180deg)');
+    });
+    $('.bookResume').click(function(){
+        $('.bookPage').css('transform', 'rotateY(-180deg)');
+    }); 
+}
